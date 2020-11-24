ca01f769260eee9ff1a59445727b280b4f829487
gel1123 <atno.14@gmail.com>
2020-11-24 14:11:17 +0900

    first commit

---
+++ 01_Hello.java
@@ -0,0 +1,11 @@
+class Hello
+{
+  public static void main(String[] args)
+  {
+    if (args == null || args.length < 1)  {
+	    System.out.println("Hello!");
+    } else {
+	    System.out.println("Hello "+args[0]+"!");
+    }
+  }
+}
---
+++ 02_Reg.java
@@ -0,0 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Reg {
+
+    public static void main(String[] args) {
+	if (args == null || args.length < 1)
+		System.out.println("No arguments");
+
+        String regexp =   "# You can also write comments.\n"
+			+ "[^0-9]+";
+        Pattern pattern = Pattern.compile(regexp, Pattern.COMMENTS);
+        Matcher matcher = pattern.matcher(args[0]);
+
+        System.out.println(matcher.matches());
+	System.out.println(matcher.find());
+	System.out.println(matcher.replaceAll(""));
+    }
+}
