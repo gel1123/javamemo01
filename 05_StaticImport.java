f30134638edb9feb1de4186a2ba53ea63e1a7905
faable01 <atno.14@gmail.com>
2020-12-29 08:57:29 +0900

    studying 'static import'

---
+++ 05_StaticImport.java
@@ -0,0 +1,9 @@
+import static pac1.Tekito.MES;
+import static pac1.Tekito.hello;
+
+class UseStaticImport {
+	public static void main(String[] args) {
+		System.out.println(MES);
+		System.out.println(hello());
+	}
+}
---
+++ pac1/Tekito.java
@@ -0,0 +1,6 @@
+package pac1;
+
+public class Tekito {
+	public static final String MES = "kore ha tekito-na-class desu.";
+	public static String hello() { return "Huh?"; }
+}
