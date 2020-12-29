3f1804583255ab61e1466f6aba9c84473e3ee5ab
faable01 <atno.14@gmail.com>
2020-12-29 09:31:07 +0900

    studying 'static import'

--- 05_StaticImport.java
+++ 05_StaticImport.java
@@ -1,7 +1,13 @@
 import static pac1.Tekito.MES;
-import static pac1.Tekito.hello;
+import static pac1.Tekito.hello; // <= メソッドのstatic importであっても、"hello()" とか書いたりはしない
+//import static pac1.Tekito.*; // <= "static import" でもアスタリスクでの全読み込みは可能の模様
 
 class UseStaticImport {
+
+	private final static String MES = "static importしたものと名前被りが生じた場合は、static importはなかったことになります。\n（コンパイルエラーや実行時例外も発生せず、単にコンパイル時にstatic importが無視されるとのこと）";
+
+	private final static String hello = "static importしたものと同名メンバだけで、メソッド or フィールドで食い違いがある場合は？";
+
 	public static void main(String[] args) {
 		System.out.println(MES);
 		System.out.println(hello());
