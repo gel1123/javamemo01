834883786246809f7a72a74fb2d1bad8c2840152
gel1123 <atno.14@gmail.com>
2021-01-02 23:51:36 +0900

    hoge

--- 07_File.java
+++ 07_File.java
@@ -34,17 +34,83 @@ class UseFieAPI {
 		w("-- f.listFiles() --");
 		w(f.listFiles());
 		try (
-			BufferedReader r = new BufferedReader(new FileReader("file/test02.txt"));
-			BufferedWriter w = new BufferedWriter(new FileWriter("file/test03.txt"))
+			BufferedReader r =
+				new BufferedReader(
+					new FileReader("file/test02.txt"));
+			BufferedWriter w =
+				new BufferedWriter(
+					new FileWriter("file/test03.txt"));
 		) {
 			String s = "";
 			while ((s = r.readLine()) != null) {
+				w("[r.readLine()] "+s);
 				w.write(s);
 				w.newLine();
 			}
 		} catch (IOException e) {
 			w(e);
-		} 
+		}
+		try (
+			BufferedReader r_test03 =
+				new BufferedReader(
+					new FileReader("file/test03.txt"))
+		) {
+			String s = "";
+			while ((s = r_test03.readLine()) != null) {
+				w("[r_test03.readLine()] "+s);
+			}
+			File file = new File("file/test03.txt");
+			w("-- file.canRead() --");
+			w(file.canRead());
+			w("-- file.canWrite() --");
+			w(file.canWrite());
+			w("-- file.canExecute() --");
+			w(file.canExecute());
+		} catch (IOException e) {
+			w(e);
+		}
+		try (
+			FileReader in = new FileReader("file/test02.txt");
+			FileWriter out = new FileWriter("file/test03.txt")
+		) {
+			int c = 0;
+			while ((c=in.read()) != -1) {
+				w("[read-test02.txt] " + ((char)c));
+				out.write((char)c);
+			}
+		} catch (IOException e) {
+			w(e);
+		}
+		try (
+			FileReader in = new FileReader("file/test03.txt");
+		) {
+			int c = 0;
+			while ((c=in.read()) != -1) {
+				w("[read-test03.txt] " + ((char)c));
+			}
+		} catch (IOException e) {
+			w(e);
+		}
+		try (
+			BufferedReader r = new BufferedReader(
+				new FileReader(
+					"file/test04.txt"));
+		) {
+			String s = "";
+			int count = 0;
+			while ((s = r.readLine()) != null) {
+				w("[readLine-test04.txt] " + s);
+				if (count++ == 1) {
+					w("-- r.mark(100) --");
+					r.mark(100);
+				}
+			}
+			w("-- r.reset() --");
+			r.reset();
+			w("[readLine-test04.txt] " + r.readLine());
+		} catch (IOException e) {
+			w(e);
+		}
 	}
 	public static void w(Object s) {
   if (s == null) {
