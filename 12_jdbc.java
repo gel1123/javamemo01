adf36a72f580c8dd047de30ee67cd56cba60dc6e
gel1123 <atno.14@gmail.com>
2021-01-07 23:43:18 +0900

    studying jdbc

--- 12_jdbc.java
+++ 12_jdbc.java
@@ -15,13 +15,55 @@ class App {
 
 		// JDBC 4.0
 		try (Connection c = DriverManager
-			.getConnection(URL, USER, PASS);
-			Statement s = c.createStatement()) {
-			ResultSet r = s.executeQuery("SELECT * FROM 01_free;");
+				.getConnection(URL, USER, PASS);
+			Statement s = c.createStatement(
+				ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_UPDATABLE)
+		) {
+
+			DatabaseMetaData m = c.getMetaData();
+			w("[[ is supported the type of 'forward-only'? ]]");
+			w(m.supportsResultSetType(
+				ResultSet.TYPE_FORWARD_ONLY));
+			w("[[ is supported the type of 'scroll insensitive'? ]]");
+			w(m.supportsResultSetType(
+				ResultSet.TYPE_SCROLL_INSENSITIVE));
+			w("[[ is supported the type of 'scroll sensitive'? ]]");
+			w(m.supportsResultSetType(
+				ResultSet.TYPE_SCROLL_SENSITIVE));
+
+			ResultSet r = s.executeQuery(
+				"SELECT * FROM 01_free;");
+
+			w("-- r.getType() --");
+			final int type;
+			w(type = r.getType());
+			if (ResultSet.TYPE_FORWARD_ONLY == type) {
+				w("the type of 'forward only'");
+			} else if (ResultSet.TYPE_SCROLL_INSENSITIVE == type) {
+				w("the type of 'scroll-insensitive'");
+			} else if (ResultSet.TYPE_SCROLL_SENSITIVE == type) {
+				w("the type of 'scroll-sensitive'");
+			} else {
+				w("unknown type");
+			}
+			if (r.isBeforeFirst()) w("<< r.isBeforeFirst >>");
+			w("-- while (r.next()) --");
 			while (r.next()) {
+				if (r.isFirst()) w("<< r.isFirst >>");
+				if (r.isLast()) w("<< r.isLast >>");
 				w(r.getInt("id"));
 				w(r.getString("name"));
 			}
+			if (r.isAfterLast()) w("<< r.isAfterLast >>");
+			w("-- while (r.previous()) --");
+			while (r.previous()) {
+				if (r.isFirst()) w("<< r.isFirst >>");
+				if (r.isLast()) w("<< r.isLast >>");
+				w(r.getInt("id"));
+				w(r.getString("name"));
+			}
+
 			int i = s.executeUpdate(
 				"INSERT INTO 01_free VALUE (4, 'hoge');");
 			w("[executeUpdate] "+ i);
@@ -31,10 +73,36 @@ class App {
 			w("[getUpdateCount()] " + s.getUpdateCount());
 			ResultSet r2 = s.getResultSet();
 			w("[getResultSet()]");
-			while (r2.next()) {
+			while (r2 != null && r2.next()) {
 				w(r2.getInt("id"));
 				w(r2.getString("name"));
 			}
+			try {
+				while (r.previous()) {
+					w(r.getInt("id"));
+					w(r.getString("name"));
+				}
+			} catch (SQLException e) {
+				w("---- error ----");
+				w("The variable [r] is already closed.");
+				w(e);
+			}
+			w("---- SELECT * FROM table; ----");
+			r = s.executeQuery("SELECT * FROM 01_free;");
+			while (r.next()) w(
+				r.getInt("id") + ": " + r.getString("name"));
+			r.absolute(3);
+			w("---- deleteRow() ----");
+			while (r.next()) {
+				w("[try delete-row] "
+					+ r.getInt("id") + ": " + r.getString("name"));
+				r.deleteRow();
+			}
+			w("---- SELECT * FROM table; ----");
+			r = s.executeQuery("SELECT * FROM 01_free;");
+			while (r.next()) w(
+				r.getInt("id") + ": " + r.getString("name"));
+
 		} catch (Exception e) {
 			w(e);
 		}
