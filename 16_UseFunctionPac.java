1e44c97bf880450c16690a34ee90fa9a663f0fca
gel1123 <atno.14@gmail.com>
2021-01-09 15:46:01 +0900

    studying about 'package java.util.function'

--- 16_UseFunctionPac.java
+++ 16_UseFunctionPac.java
@@ -36,7 +36,39 @@ class App {
 		w_bifunction((s1, s2) -> (s1+s2).length(),
 			"Bi", "Function");
 		w_unaryoperator(i -> i*i);
+
+		//_____________________________________
+
+		w("---- exe BinaryOperator ----");
+		BinaryOperator<String> b = (s1, s2) -> s1+s2;
+		w(b.apply("Bi", "Ope"));
+
+		w("---- use 'Method Reference' on lambda ----");
+		Consumer<String> c = System.out::println;
+		c.accept("[SAM-Signature] void xxx(String)");
+		
+		w("---- use 'Constructor Reference' on lambda ----");
+		Function<String, Integer> f = Integer::new;
+		w(f.apply("10"));
+
+		w("---- use 'Instance Method Ref' on lambda ----");
+		f = String::length; // Integer xxx(String)
+		w(f.apply("instance")); // Integer str.xxx()
+		w(new Function<String, Integer>() {
+			@Override
+			public Integer apply(String s) {
+				return s.length();
+			}
+		}.apply("anonymous"));
+
+		w("---- use 'Object Method Ref' on lambda ----");
+		String str = "obj";
+		Function<String, String> f2 = str::concat;
+		w(f2.apply(" met ref"));
 	}
+
+	//_______________________________________
+
 	public static void w_supplier(Supplier<String> s) {
 		w("---- exe Supplier ----");
 		w(s.get());
